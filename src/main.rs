ee65dee479f2f1d8bbcfa025b9e06a63ae9fd339
Svitoslav Burzak <133600024+oslavq@users.noreply.github.com>
2025-07-09 21:42:00 +0330

    Close #7 and create a library

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashSet, env::args};
+use std::env::args;
+
+use vietta_solver::factors_find::find_factors;
 
 fn main() {
     let args: Vec<String> = args().collect();
@@ -18,16 +20,6 @@ fn solve(p: i32, q: i32) -> [i32; 2] {
             if enc_p[0] + enc_p[1] == -p { [r1, r2] = enc_p;};}};
     if r1 > r2 { [r2, r1] } else { [r1, r2] } // return roots in incremental order, so the smallest one always comes first
     }
-
-fn find_factors(num: i32) -> Vec<[i32; 2]> {
-    let mut vector: Vec<[i32; 2]> = vec![[1, num.abs()]];
-    let mut used_nums: HashSet<i32> = HashSet::new(); // avoid repetition
-    for i in 2..=((num as f32 / 2f32).ceil() as i32 + 1).abs() { // iterate over nums from 2 to the half of q
-        if used_nums.contains(&i) || i == num.abs() { break } // if i is a number on position 2 from any previous entry
-        if (num.abs() % i) == 0 {vector.push([i, num.abs() / i]); // if i turns out to be a factor if num
-            used_nums.insert(num.abs() / i);}} // save second factor
-    return vector
-}
 
 fn encode(pair: [i32; 2]) -> [[i32;2]; 4] {
        [[pair[0],   pair[1]],
