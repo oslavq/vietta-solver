146a5454cebe80c34eee6da5d94aa76ff6c570b0
Svitoslav Burzak <133600024+oslavq@users.noreply.github.com>
2025-07-13 13:57:50 +0330

    Custom data type for solution to close #3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::env::args;
 
-use vietta_solver::solver::solve;
+use vietta_solver::solver::{solve, RootAmount};
 
 fn main() {
     let args: Vec<String> = args().collect();
     if args.len() < 3 { panic!("Please enter p and q as arguments (from x^2+px+q)"); }
     let result = solve(args[1].parse().unwrap(), args[2].parse().unwrap());
-    if result == [0, 0] { panic!("Could not solve for roots using the Vietta's theorem! Pass p and q as arguments from x^2+px+q"); }
-    println!("Here is your result:\nX1\t=\t{}\nX2\t=\t{}", result[0], result[1]);
+    match result.roots_found() {
+        RootAmount::None => {
+            println!("Could not solve for roots using Vietta's Theorem!");
+        }
+        RootAmount::One => {
+            println!("We found only one root!\nx1\t=\t{}", result.x1.unwrap());
+        }
+        RootAmount::Two => {
+            println!("Solved!\nx1\t=\t{}\nx2\t=\t{}", result.x1.unwrap(), result.x2.unwrap());
+        }
+    }
 }
--- src/solver.rs
+++ src/solver.rs
@@ -1,15 +1,50 @@
 use crate::factors_find::find_factors;
 
+#[derive(PartialEq, Debug)]
+pub struct Solution {
+    pub x1: Option<i32>,
+    pub x2: Option<i32>,
+}
+
+pub enum RootAmount {
+    None,
+    One,
+    Two
+}
+
+impl Solution {
+    fn new_blank() -> Solution { Self { x1: None, x2: None } }
+    fn new(x1: i32, x2: i32) -> Self { Self { x1: Some(x1), x2: Some(x2) }}
+
+    pub fn roots_found(&self) -> RootAmount {
+        if self.x1 == None && self.x2 == None {
+            RootAmount::None } else if self.x2 == None {
+                RootAmount::One
+            } else {
+                RootAmount::Two }
+    }
+}
+
 // 1.   x1*x2=q  |   x1=q/x2
 // 2.   x1+x2=-p |   x1=-p-x2
-pub fn solve(p: i32, q: i32) -> [i32; 2] {
+pub fn solve(p: i32, q: i32) -> Solution {
     let pairs_1: Vec<[i32; 2]> = find_factors(q); // pairs of numbers that satisfy the first boundary
-    let (mut r1, mut r2): (i32, i32) = (0, 0);
+    let mut sol = Solution::new_blank();
     for pair in pairs_1 { // go over first set of pairs
         for enc_p in encode(pair) { // go over each encoded pair
-            if enc_p[0] + enc_p[1] == -p { [r1, r2] = enc_p;};}};
-    if r1 > r2 { [r2, r1] } else { [r1, r2] } // return roots in incremental order, so the smallest one always comes first
-    }
+            if enc_p[0] + enc_p[1] == -p { // found a valid pair
+                if enc_p[0] == enc_p[1] { // if there is only one root
+                    sol = Solution { x1: Some(enc_p[0]), x2: None };
+                } else { // if there are two roots, sort the values
+                  if enc_p[0] < enc_p[1] {
+                    sol.x1 = Some(enc_p[0]); sol.x2 = Some(enc_p[1]);
+                  } else {
+                    sol.x1 = Some(enc_p[1]); sol.x2 = Some(enc_p[0]);
+                  }
+                }
+            };}};
+    return sol;
+}
 
 fn encode(pair: [i32; 2]) -> [[i32;2]; 4] {
        [[pair[0],   pair[1]],
@@ -25,16 +60,16 @@ fn test_encoder() {
             ([10, 5],   [[10, 5], [-10, 5], [10, -5], [-10, -5]]),
             ([-3, 12],  [[-3, 12], [3, 12], [-3, -12], [3, -12]])];
     for value in values {
-        assert_eq!(encode(value.0), value.1);}
-}
+        assert_eq!(encode(value.0), value.1);
+}}
 
 #[test]
 fn test_solver() {
-    let values: [[[i32; 2] ;2] ;4] =
-           [[[-28, 96], [4, 24]],
-            [[-5, -6], [2, 3]],
-            [[2, -120], [-12, 10]],
-            [[31, -180], [-36, 5]]];
+    let values: [([i32; 2], Solution) ;4] =
+        [([-28, 96], Solution::new(4, 24)),
+        ([-5, -6], Solution::new(2, 3)),
+        ([2, -120], Solution::new(-12, 10)),
+        ([31, -180], Solution::new(-36, 5))];
     for val in values {
-        assert_eq!(solve(val[0][0], val[0][1]), val[1]);}
-}
+        assert_eq!(solve(val.0[0], val.0[1]), val.1);
+}}
