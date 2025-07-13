6c8c147457cfb515f31ce7edd5badf1423b88c93
Svitoslav Burzak <133600024+oslavq@users.noreply.github.com>
2025-07-13 14:48:01 +0330

    Improve argument parsing to close #4

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
+use core::panic;
 use std::env::args;
 
 use vietta_solver::solver::{solve, RootAmount};
 
 fn main() {
     let args: Vec<String> = args().collect();
-    if args.len() < 3 { panic!("Please enter p and q as arguments (from x^2+px+q)"); }
-    let result = solve(args[1].parse().unwrap(), args[2].parse().unwrap());
+    if args.len() < 3 || args.len() > 3 { panic!("Please enter p and q as arguments\n(from a quadratic expression like x^2+px+q)"); }
+    let (mut p, mut q): (i32, i32) = (0, 0);
+    let mut iteration: u8 = 0;
+    for arg in &args[1..=2] {
+        iteration += 1;
+        match arg.parse::<i32>() {
+            Ok(num) => { 
+                if iteration == 1 { p = num; } else {
+                    q = num; }
+             }
+            Err(e) => {
+                panic!("Could not parse argument Num.{}: {}\nAre you sure this is a valid integer?\n\n{}", iteration, arg, e);
+            }
+        }
+    }
+    if p == 0 { panic!("p cannot be zero"); }
+
+    let result = solve(p, q);
     match result.roots_found() {
         RootAmount::None => {
-            println!("Could not solve for roots using Vietta's Theorem!");
+            println!("Could not solve for roots using Vietta's Theorem!\nYour equation most likely has zero roots");
         }
         RootAmount::One => {
             println!("We found only one root!\nx1\t=\t{}", result.x1.unwrap());
