2074daa0b9b81690a7fad55bbc847378755b83b0
Svitoslav Burzak <133600024+oslavq@users.noreply.github.com>
2025-07-14 16:22:46 +0330

    Add "step" logic to find_factors() to close #5

--- src/factors_find.rs
+++ src/factors_find.rs
@@ -1,12 +1,19 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, vec};
 
 pub fn find_factors(num: i32) -> Vec<[i32; 2]> {
     let mut vector: Vec<[i32; 2]> = vec![[1, num.abs()]];
     let mut used_nums: HashSet<i32> = HashSet::new(); // avoid repetition
+
+    let mut curr_step: i32 = 1; // see #5
+
     for i in 2..=((num as f32 / 2f32).ceil() as i32 + 1).abs() { // iterate over nums from 2 to the half of q
+        if i % curr_step != 0 { continue; }
         if used_nums.contains(&i) || i == num.abs() { break } // if i is a number on position 2 from any previous entry
         if (num.abs() % i) == 0 {vector.push([i, num.abs() / i]); // if i turns out to be a factor if num
-            used_nums.insert(num.abs() / i);}} // save second factor
+            used_nums.insert(num.abs() / i); // save second factor
+            let v_len: usize = vector.len();
+            curr_step = vector[v_len-1][0] - vector.get(v_len-2).unwrap_or(&[1, 1])[0];
+        }}
     return vector
 }
 
