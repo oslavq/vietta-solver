2b8190c30062d126fd28a5703f1b57f2dca8bdd1
Svitoslav Burzak <133600024+oslavq@users.noreply.github.com>
2025-07-24 08:47:42 +0330

    Panic if solution validation failed to close #25

--- src/bin/root_solver.rs
+++ src/bin/root_solver.rs
@@ -1,11 +1,13 @@
 use core::panic;
 use std::env::args;
 
-use vietta_solver::solver::{solve, RootAmount};
+use vietta_solver::solver::{solve, RootAmount, Solution};
+use vietta_solver::validate::validate;
 
 fn main() {
     let args: Vec<String> = args().collect();
-    if args.len() < 3 || args.len() > 3 { panic!("Please enter p and q as arguments\n(from a quadratic expression like x^2+px+q)"); }
+    dbg!(args.len());
+    if args.len() < 3 || args.len() > 4 { panic!("Please enter p and q as arguments\n(from a quadratic expression like x^2+px+q)\nYou can also use optional argument --no-check to not perform validation"); }
     let (mut p, mut q): (i32, i32) = (0, 0);
     let mut iteration: u8 = 0;
     for arg in &args[1..=2] {
@@ -21,6 +23,7 @@ fn main() {
         }
     }
     if p == 0 { panic!("p cannot be zero"); }
+    let validation_enabled: bool = if args.get(3) == Some(&"--no-check".to_string()) { false } else { false };
 
     let result = solve(p, q);
     match result.roots_found() {
@@ -28,10 +31,19 @@ fn main() {
             println!("Could not solve for roots using Vietta's Theorem!\nYour equation most likely has zero roots");
         }
         RootAmount::One => {
+            if validation_enabled { check(p, q, &result); }
             println!("We found only one root!\nx1\t=\t{}", result.x1.unwrap());
         }
         RootAmount::Two => {
+            if validation_enabled { check(p, q, &result); }
             println!("Solved!\nx1\t=\t{}\nx2\t=\t{}", result.x1.unwrap(), result.x2.unwrap());
         }
+    }
+}
+
+fn check(p: i32, q: i32, sol: &Solution) {
+    let pass: bool = validate(p, q, &sol);
+    if !pass {
+        panic!("Something went wrong on our end.\nWe found the root(s) but their validation failed\nRun with --no-check to ignore");
     }
 }
